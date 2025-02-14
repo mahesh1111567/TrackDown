24ca5b32cad73cde3f8fe1246d54a0fbd2e65284
mahesh1111567 <anilyadav780399+1111@gmail.com>
2025-02-14 07:58:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://check.onrender.com";
+var hostURL="https://check-6grc.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
