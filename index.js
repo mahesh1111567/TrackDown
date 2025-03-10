2e9dc5973a8a28bd6f51258e64c5dfce0ef0ccc0
mahesh1111567 <anilyadav780399+1111@gmail.com>
2025-03-10 08:39:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://gifts-for-you-u7th.onrender.com";
+var hostURL="https://trackdown-bxrh.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
